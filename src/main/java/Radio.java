66cd099a3b9de51e2381f8addba05de23d6346fc
Victor Petrunin <petruninviktor9464@gmail.com>
2022-10-21 14:27:29 +0300

    second commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class Radio {
+public class Radio {
+    protected int currentStation;
+    protected int currentVolume;
+
+    protected int maxStation;
+
+    public Radio() {
+        this.maxStation = 10;
+    }
+
+    public Radio(int stationsCount) {
+        this.maxStation = stationsCount - 1;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 1) {
+            return;
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void nextVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = 1;
+        }
+    }
+
+    public void prevVolume() {
+        if (currentVolume > 1) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = 100;
+        }
+    }
+
 }
